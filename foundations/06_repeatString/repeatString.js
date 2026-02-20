b6d590cad5a63e5328729d1c1f8c6646bdd14c8c
phoenixwright-creator <pwright725@Xubuntu.myguest.virtualbox.org>
2026-02-20 16:55:32 +0100

    Complete 6th js exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let newString = "";
+    if(num >= 0) {
+        for (let i = 0; i<num; i++) {
+            newString = newString.concat("", string);
+        }
+    }
+    else {
+        return 'ERROR';
+    }
+    return newString;
 
 };
 
